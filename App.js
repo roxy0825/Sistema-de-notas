46a644391b4aeb15001e7f969ccdb8d0a64a8db5
betzeafer <santiagoheerrera@gmail.com>
2023-03-16 20:06:03 -0500

    jueves

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container }>
+      <View style={[styles.container ,{ flex:1}]}>
+        <Text style={{fontWeight:'bold'}}>Sistema de notas</Text>
+      </View>
+
+
+      <View >
+     <Text style={{fontWeight:'bold'}}>Identificacion</Text>
+     <Text style={{fontWeight:'bold'}}>Identificacion</Text>
+     <Text style={{fontWeight:'bold'}}>Identificacion</Text>
+     <Text style={{fontWeight:'bold'}}>Identificacion</Text>
+     <Text style={{fontWeight:'bold'}}>Identificacion</Text>
+     </View>
+     
+      
+      
       <StatusBar style="auto" />
     </View>
+    
+    
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +37,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+   
+    
+
   },
 });
